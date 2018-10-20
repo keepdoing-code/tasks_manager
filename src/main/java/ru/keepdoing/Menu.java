3cc4bcc0140556f68d56f7906902496d422a1fc2
stritron <finalroot@gmail.com>
2018-10-21 02:21:00 +0300

    Minimal working version of universal menu. Bigger TODO list. Many small corrections.

--- src/main/java/ru/keepdoing/Menu.java
+++ src/main/java/ru/keepdoing/Menu.java
@@ -2,15 +2,6 @@ package ru.keepdoing;
 
 public class Menu {
 
-    public static final String MAIN_MENU = new MenuBuilder()
-            .addLine("Select what your want to do?")
-            .addTab("1. Show Tasks")
-            .addTab("2. Tasks manager")
-            .addTab("3. Types and Statuses")
-            .addTab("4. Exit")
-            .add("> ")
-            .getString();
-
     public static final String DEV_MENU = new MenuBuilder()
             .fillPattern("-", 20)
             .addLine("Develop menu")
